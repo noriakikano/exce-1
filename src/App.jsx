255012c1c4ccaa73a9badece686d7bede9b40d3d
noriakikano <109402633+noriakikano@users.noreply.github.com>
2022-07-19 18:03:32 +0900

    デザイン完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,39 @@
 import React, { StrictMode } from "react";
 import ReactDom from "react-dom";
 // import ColufulMessage from "./componets/ColourfulMessage";
+import "./style.css";
 
 const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="todoを追加" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のtodo</p>
+      <div className="incomplete-area">
+        <p className="title">未完了のtodo</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>あああ</li>
             <button>完了</button>
             <button>削除</button>
           </div>
-          <div>
+          <div className="list-row">
             <li>あああ</li>
             <button>完了</button>
             <button>削除</button>
           </div>
         </ul>
       </div>
-      <div>
-        <p>完了のtodo</p>
+      <div className="complete-area">
+        <p className="title">完了のtodo</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>あああ</li>
             <button>完了</button>
             <button>削除</button>
           </div>
-          <div>
+          <div className="list-row">
             <li>あああ</li>
             <button>完了</button>
             <button>削除</button>
